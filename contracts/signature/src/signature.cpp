39a0c9772b6d5b40f415d06dbc3fba98e552cf54
Luis Angel <tortoladev@gmail.com>
2018-11-25 09:16:08 -0600

    fix Signature function

--- contracts/signature/src/signature.cpp
+++ contracts/signature/src/signature.cpp
@@ -21,17 +21,17 @@ class [[eosio::contract]] vote : public eosio::contract {
     using contract::contract;
     addressbook(string citizen_uid, string volunteer_id,  datastream<const char*> ds):contract(citizen_uid, volunteer_id, ds) {}
 
-    [[eosio::action]]
-    void upsert(std::string citizen_uid, std::string volunteer_id, std::string image_hash) {
+    
+    void insert(std::string citizen_uid, std::string volunteer_id, std::string image_hash) {
         address_index addresses(_code, citizen_uid);
         auto iterator = addresses.find(citizen_uid);
         if( iterator == addresses.end() )
         {
             addresses.emplace(citizen_uid, [&]( auto& row ) {
-            row.key = citizen_uid;
+            row.citizen_uid = citizen_uid;
             row.volunteer_id = volunteer_id;
             row.image_hash = image_hash;
-        });
+            });
         }
         else {
             //The user is in the table
@@ -52,4 +52,4 @@ class [[eosio::contract]] vote : public eosio::contract {
 };
 
 
-EOSIO_DISPATCH( vote, (upsert))
+EOSIO_DISPATCH( vote, (insert))
