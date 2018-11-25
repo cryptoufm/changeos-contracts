9d3eb653df7dd9a735e4857724319bf42661d3ca
Luis Angel <tortoladev@gmail.com>
2018-11-25 02:02:32 -0600

    Verify dpi function modified

--- contracts/signature/src/signature.cpp
+++ contracts/signature/src/signature.cpp
@@ -12,25 +12,38 @@
 using namespace eosio;
 using namespace std;
 
+class Signature {
+    public:
+        Signature() {}
+        Signature(std::string volunteerid, std::string imagehash) : volunteerid(volunteerid), imagehash(imagehash) {}
+        std::string volunteerid;
+        std::string imagehash; 
+};
+
+
 class vote : public eosio::contract {
 
     public:
         using contract::contract;
 
+        std::map<std::string, Obj> registry; 
+
         [[eosio::action]]
-        void addsignature( string dpi ) {
-            print( "DPI received: ", dpi );
-            verifydpi(dpi);
+        void addsignature( string volunteerid, string citizenuid, string imagehash ) {
+            print( "DPI received: ", volunteerid, citizenuid, imagehash );
+            if (verifydpi(citizenuid)) {
+                logger_info( "SUCCESS: DPI added: ", dpi);
+                registry.insert(std::map<std::string, Obj>::value_type(citizenuid, Obj(volunteerid,imagehash)));
+            } 
+            else {
+                logger_info( "FAILED: DPI not added: ", dpi);
+            }
         }
 
         bool verifydpi( string dpi ) {
-            logger_info( "debug dpi: ", dpi);
+            print( "DPI received in VERIFYDPI" );
             return true;
        }
-
-
-
-
 
 };
 
