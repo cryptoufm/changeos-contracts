5d2ff4c117652d68b6815bd8138d56d3441e5a80
Juanro Alvarado <juanroalvarado@Juanros-MacBook-Pro.local>
2018-11-25 12:46:36 -0600

    fixes with medium tutorial

--- contracts/signature/src/signature.cpp
+++ contracts/signature/src/signature.cpp
@@ -12,72 +12,40 @@ using namespace std;
 
 using namespace eosio;
 
-class vote : public eosio::contract {
+class referendum : public eosio::contract {
 
-  public:   
+    public:   
 
-    using contract::contract;
-    // addressbook(string citizen_uid, string volunteer_id,  datastream<const char*> ds):contract(citizen_uid, volunteer_id, ds) {} 
-    
-    /// @abi action
-    void insert(uint64_t citizen_uid, std::string volunteer_id, std::string image_hash) {
+        using contract::contract;
 
+        //referendum(string citizen_uid, string volunteer_id,  datastream<const char*> ds):contract(citizen_uid, volunteer_id, ds) {} 
         
+        // @abi action
+        void add_signature(uint64_t citizen_uid, std::string volunteer_id, std::string image_hash) {
 
-        petition_index addresses(petition_sign(citizen_uid));
-
-        auto iterator = addresses.find(citizen_uid);
-
-        if( iter == addresses.end() )
-        {
-            addresses.emplace(citizen_uid,[&](auto& row) {
-                
-                row.citizen_uid = citizen_uid;
-                row.volunteer_id = volunteer_id;
-                row.image_hash = image_hash;
-            });
+            auto new_sign = petition_index.emplace( _self, [&]( auto& petition_sign)){
+                petition_sign.citizen_uid = citizen_uid;
+                petition_sign.volunteer_id = volunteer_id;
+                petition_sign.image_hash = image_hash;
+            
+            }        
         }
-        else {
-            //The user is in the table
-             print("UID already voted\t");
-        }
-    }
+
+        private:
+
+            // @abi table petition_sign i64
+            struct petition_sign {
+                name citizen_uid;
+                std::string volunteer_id;
+                std::string image_hash;
+
+                uint64_t primary_key() const { return citizen_uid.value; }
+
+                EOSLIB_SERIALIZE( petition_sign, (citizen_uid)(volunteer_id)(image_hash))
+            };
+        
+            multi_index<N(petition_sign), petition_sign> petition_index;
     
-    // void getInfo(std::string citizen_uid) {
-
-    //     //address_index addresses(signature(citizen_uid));
-
-    //     _signature signature(_self, _self);
-
-    //     auto sig = signature.find(citizen_uid);
-
-    //     if( sig != signature.end() )
-    //     {
-    //         print(sig->citizen_uid);
-    //         print(sig->volunteer_id);
-    //         print(sig->image_hash);
-    //     }
-    //     else {
-    //         //The user is in the table
-    //          print("Signature does not exist\t");
-    //     }
-    // }
-
-    /// @abi table petition_sign i64
-    struct petition_sign {
-        uint64_t citizen_uid;
-        std::string volunteer_id;
-        std::string image_hash;
-
-        uint64_t primary_key() const { return citizen_uid; }
-
-        EOSLIB_SERIALIZE(petition_sign,(citizen_uid)(volunteer_id)(image_hash))
-
-    };
-  
-    typedef eosio::multi_index<"petition"_n, petition_sign> petition_index;
-    
-
 };
 
-EOSIO_ABI( vote, (insert))
+EOSIO_ABI( vote, (add_signature))
