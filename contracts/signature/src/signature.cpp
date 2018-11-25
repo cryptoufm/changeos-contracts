317f4c529044cb2a72ad868e0f20b0d1a9b791e6
Luis Angel <tortoladev@gmail.com>
2018-11-25 09:45:34 -0600

    fix Signature function

--- contracts/signature/src/signature.cpp
+++ contracts/signature/src/signature.cpp
@@ -48,7 +48,7 @@ class vote : public eosio::contract {
       std::string primary_key() const { return citizen_uid; }
     };
   
-    typedef eosio::multi_index<string, signature> address_index;
+    typedef eosio::multi_index<N(signature), signature> address_index;
 };
 
 
