5d0504b48ee69bd3d16692eb1aad09d08458dff3
Edgar Godinez <edgarenrique.godinez@gmail.com>
2018-11-25 13:55:00 -0600

    bendi

--- build_deploy_test.py
+++ build_deploy_test.py
@@ -1,6 +1,8 @@
 from eosfactory.eosf import *
 
-contract = ContractBuilder("signature")
+
+contract = ContractBuilder("referendum")
+
 contract.build()
 reset()
 create_master_account("master")
@@ -8,5 +10,7 @@ create_account("host", master)
 contract = Contract(host, contract.path())
 contract.deploy()
 
+print("----ContractTest----")
 contract.push_action("insert", {"citizen_uid": "3001973810101", "volunteer_id": "JRBAGUR", "image_hash": "QmdodXR3WWQGxmUmQsZy9PjuzYuUwAiEgCMif92ChLq2sJ"}, permission=host)
 contract.push_action("insert", {"citizen_uid": "3001973810101", "volunteer_id": "JUANRO", "image_hash": "xmUmQzYuU3WWQGxmUmQQmdodXR3WWQGsZy9Pjuif92ChLq2sJ"}, permission=host)
+print("----/ContractTest----")
