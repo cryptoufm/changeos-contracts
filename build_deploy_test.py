95b3c599e566052c3d34e41d5677831c364704b4
Juanro Alvarado <juanroalvarado@Juanros-MacBook-Pro.local>
2018-11-25 14:33:56 -0600

    assert test

--- build_deploy_test.py
+++ build_deploy_test.py
@@ -13,7 +13,7 @@ contract.deploy()
 print("----ContractTest----")
 
 contract.push_action("insert", {"citizen_uid": "3001973810101", "volunteer_id": "JRBAGUR", "image_hash": "QmdodXR3WWQGxmUmQsZy9PjuzYuUwAiEgCMif92ChLq2sJ"}, permission=host)
-contract.push_action("insert", {"citizen_uid": "3001973810101", "volunteer_id": "JRBAGUR", "image_hash": "QmdodXR3WWQGxmUmQsZy9PjuzYuUwAiEgCMif92ChLq2sJ"}, permission=host)
+contract.push_action("insert", {"citizen_uid": "3001973810101", "volunteer_id": "JUANRO", "image_hash": "QmdodXR3Zy9PjuzYuUwAiEgCMif92ChWWQGxmUmQsLq2sJ"}, permission=host)
 contract.push_action("insert", {"citizen_uid": "3003911290101", "volunteer_id": "JRBAGUR", "image_hash": "QmdodXR3WWQGxmUmQsZy9PjuzYuUwAiEgCMif92ChLq2sJ"}, permission=host)
 
 contract.push_action("retrieve", {"citizen_uid": "3001973810101"} , permission=host)
